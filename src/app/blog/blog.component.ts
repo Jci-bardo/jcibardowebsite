1af55fa8574524d0daac34dd0123a7411fd237a6
anas4444 <anas.elabed@etudiant-fst.utm.tn>
2023-09-22 20:45:04 +0100

    changes

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BlogComponent implements OnInit {
 
   blogs = [
-    {id:"ecovertemarchevert", titre:"Marché-vert: Foire Bio", place:"Le Bardo", date:"24 Septembre, 2023", img:"./assets/img/programmes/projetslocaux/marche-vert.jpg"},
+    {id:"ecovertemarchevert", titre:"Marché-vert : Foire Bio", place:"Le Bardo", date:"24 Septembre, 2023", img:"./assets/img/programmes/projetslocaux/marche-vert.jpg"},
     {id:"recrutementksarsaid2", titre:"Recrutement citoyens ksar-Said", place:"Municipalité du Bardo", date:"03 Septembre, 2023", img:"./assets/img/programmes/projetslocaux/recrute2.jpg"},
     {id:"journeeenseignants", titre:"Journée des enseignants", place:"Bureau d'emploi de Bardo", date:"21 Août, 2023", img:"./assets/img/programmes/projetslocaux/journée-ens.jpg"},
   ]
